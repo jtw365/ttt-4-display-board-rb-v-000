0e3bde8620d7baab8cf6f40dcab42843a9cdff5d
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-01 00:49:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,5 +13,3 @@ def display_board(board)
 
 puts "#{board[0]}", "#{board[1]}", "#{board[2]}", "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}", "#{board[7]}", "#{board[8]}"
   end
-def display_board("X","X","X", " ", " ", " ", " ", " ", " ")
-end
