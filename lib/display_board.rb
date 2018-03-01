0d09ce4bcdcbec4deca12eb4858be30f93ee082e
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-01 00:18:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def row
+  ["   " "|" "   " "|" "   "]
+end
+
+
+
+def display_board(board)
+  dash = ["-----------"]
+  puts row
+  puts dash
+  puts row
+  puts dash
+  puts row
+end
