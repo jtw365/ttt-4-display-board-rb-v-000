cf8f68b9a83dd35ff5881fa28e579ee512d8df54
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-01 01:03:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board = ["X", "X", "X", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts ["-----------"]
-  puts "   | #{board[4} |   "
+  puts "   |   |   "
   puts ["-----------"]
   puts "   |   |   "
 
